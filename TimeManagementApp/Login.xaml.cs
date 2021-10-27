cff19bf1b153d7060e3cea560505d0265c01179c
blair <blairknox02@gmail.com>
2021-10-27 12:45:24 +0200

    hellio

---
+++ TimeManagementApp/Login.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.Entity;
+
+
+namespace TimeManagementApp
+{
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            UserLogin();
+        }
+        
+        public async void UserLogin()
+        {
+            string Username = txbUsername.Text;
+            string Password = PassHash.EncodePasswordToBase64(txbPassword.Password);
+
+            if (txbUsername.Text == "" || txbPassword.Password == "")
+            {
+                MessageBox.Show("Fill in all the fields");
+            }
+            else
+            {
+                try
+                {
+                    User user = await db.Users.Where(u => u.Username.Equals(Username) && u.Password.Equals(Password)).FirstAsync();
+                    MessageBox.Show("Logged in successfully !");
+                    MainWindow window = new MainWindow(user.UserID);
+                    this.Hide();
+                    window.ShowDialog();
+                    this.ShowDialog();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Invalid login credentials");
+                }
+            }
+        }
+        
+        public async void UserRegistration()
+        {
+            if (txbUsername.Text != null || txbPassword.Password != null)
+            {
+                try
+                {
+                    User newUser = new User();
+                    newUser.Username = txbUsername.Text;
+                    newUser.Password = PassHash.EncodePasswordToBase64(txbPassword.Password);
+                    db.Users.Add(newUser);
+                    await db.SaveChangesAsync();
+                    MessageBox.Show("\n successfully registered. You may login", "Message");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please fill in all fields", "Error Message");
+            }
+        }
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            UserRegistration();
+        }
+    }
+}
---
+++ TimeManagementApp/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TimeManagementApp
+{
+    public class User
+    {
+        public User()
+        {
+            this.Calculation = new HashSet<Calculation>();
+        }
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        
+        public virtual ICollection<Calculation> Calculation { get; set; }  
+    }
+    
+}
